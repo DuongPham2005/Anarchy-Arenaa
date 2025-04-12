b6255ab0f0c85ae67403e884f08d8aae8ff8fc0e
DuongPham2005 <duongptps40672@gmail.com>
2025-04-13 01:58:00 +0700

    Done MenuManager

--- Assets/Duong/Scripts/MenuManager.cs
+++ Assets/Duong/Scripts/MenuManager.cs
@@ -2,15 +2,44 @@ using UnityEngine;
 
 public class MenuManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public static MenuManager Instance;
+
+    [SerializeField] Menu[] menus;
+
+    void Awake()
     {
-        
+        Instance = this;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OpenMenu(string menuName)
     {
-        
+        for (int i = 0; i < menus.Length; i++)
+        {
+            if (menus[i].menuName == menuName)
+            {
+                menus[i].Open();
+            }
+            else if (menus[i].open)
+            {
+                CloseMenu(menus[i]);
+            }
+        }
+    }
+
+    public void OpenMenu(Menu menu)
+    {
+        for (int i = 0; i < menus.Length; i++)
+        {
+            if (menus[i].open)
+            {
+                CloseMenu(menus[i]);
+            }
+        }
+        menu.Open();
+    }
+
+    public void CloseMenu(Menu menu)
+    {
+        menu.Close();
     }
 }
