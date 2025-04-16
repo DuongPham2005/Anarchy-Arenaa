49908599bb191b8a07e93da40ace8f47b175f750
DuongPham2005 <duongptps40672@gmail.com>
2025-04-17 01:41:40 +0700

    Ammo Check

--- Assets/Duong/Scripts/Weapon.cs
+++ Assets/Duong/Scripts/Weapon.cs
@@ -1,4 +1,5 @@
 using Photon.Pun;
+using TMPro;
 using UnityEngine;
 
 public class Weapon : MonoBehaviour
@@ -14,20 +15,61 @@ public class Weapon : MonoBehaviour
 
     private float nextFire;
 
+    [Header("Ammo")]
+    public int mag = 5;
+
+    public int ammo = 30;
+    public int magAmmo = 30;
+
+    [Header("UI")]
+    public TextMeshProUGUI magText;
+    public TextMeshProUGUI ammoText;
+
+
+    void Start()
+    {
+        magText.text = mag.ToString();
+        ammoText.text = ammo + "/" + magAmmo;
+    }
     // Update is called once per frame
     void Update()
     {
         if (nextFire > 0)
+        {
             nextFire -= Time.deltaTime;
+        }
 
-        if (Input.GetButton("Fire1") && nextFire <= 0)
+        if (Input.GetButton("Fire1") && nextFire <= 0 && ammo > 0)
         {
             nextFire = 1 / fireRate;
+
+            ammo--;
+
+            magText.text = mag.ToString();
+            ammoText.text = ammo + "/" + magAmmo;
+
             Fire();
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Reload();
         }
     }
 
-    // usage
+
+    void Reload()
+    {
+        if(mag > 0)
+        {
+            mag--;
+
+            ammo = magAmmo;
+        }
+
+        magText.text = mag.ToString();
+        ammoText.text = ammo + "/" + magAmmo;
+    }
     void Fire()
     {
         Ray ray = new Ray(origin: camera.transform.position, direction: camera.transform.forward);
