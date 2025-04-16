fdc33be19fda15686df95f5ae5ad2ca66a8c5920
DuongPham2005 <duongptps40672@gmail.com>
2025-04-16 09:57:46 +0700

    Walk update

--- Assets/Duong/Scripts/Movement.cs
+++ Assets/Duong/Scripts/Movement.cs
@@ -3,10 +3,13 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     public float walkSpeed = 4f;
+    public float sprintSpeed = 14f;
     public float maxVelocityChange = 10f;
 
     private Vector2 input;
     private Rigidbody rb;
+
+    private bool sprinting;
 
     void Start()
     {
@@ -17,6 +20,8 @@ public class Movement : MonoBehaviour
     {
         input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         input.Normalize();
+
+        sprinting = Input.GetButton("sprint");
     }
 
     void FixedUpdate()
@@ -28,21 +33,25 @@ public class Movement : MonoBehaviour
     {
         Vector3 targetVelocity = new Vector3(input.x, 0, input.y);
         targetVelocity = transform.TransformDirection(targetVelocity);
+
         targetVelocity *= _speed;
 
         Vector3 velocity = rb.linearVelocity;
 
-        if (input.magnitude > 0.5f)
+        if(input.magnitude > 0.5f)
         {
             Vector3 velocityChange = targetVelocity - velocity;
+
             velocityChange.x = Mathf.Clamp(velocityChange.x, -maxVelocityChange, maxVelocityChange);
             velocityChange.z = Mathf.Clamp(velocityChange.z, -maxVelocityChange, maxVelocityChange);
+
             velocityChange.y = 0;
-            return velocityChange;
+
+            return (velocityChange);
         }
         else
         {
-            return Vector3.zero;
+            return new Vector3();
         }
     }
 }
--- Assets/Duong/Scripts/RoomManager.cs
+++ Assets/Duong/Scripts/RoomManager.cs
@@ -4,38 +4,37 @@ using Photon.Pun;
 public class RoomManager : MonoBehaviourPunCallbacks
 {
     public GameObject player;
-
     [Space]
     public Transform spawnPoint;
 
-    // Start is called before the first frame update
+
     void Start()
     {
-        Debug.Log(message: "Connecting..");
+        Debug.Log("Connecting...");
+
         PhotonNetwork.ConnectUsingSettings();
     }
 
     public override void OnConnectedToMaster()
     {
         base.OnConnectedToMaster();
-        Debug.Log(message: "Connected to server");
+        Debug.Log("Connected to server");
         PhotonNetwork.JoinLobby();
     }
 
     public override void OnJoinedLobby()
     {
         base.OnJoinedLobby();
-        Debug.Log(message: "We're in the lobby");
-        PhotonNetwork.JoinOrCreateRoom(roomName: "test", roomOptions: null, typedLobby: null);
+        Debug.Log("We're in the lobby");
+        PhotonNetwork.JoinOrCreateRoom("test", null, null);
     }
 
-    public override void OnCreatedRoom()
+    public override void OnJoinedRoom()
     {
-        base.OnCreatedRoom();
-
+        base.OnJoinedRoom();
         Debug.Log("We're connected and in a room!");
-
         GameObject _player = PhotonNetwork.Instantiate(player.name, spawnPoint.position, Quaternion.identity);
+
         _player.GetComponent<PlayerSetup>().IsLocalPlayer();
     }
 }
